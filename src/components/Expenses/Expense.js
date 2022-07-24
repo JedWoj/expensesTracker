6d6f93333c158fc3d3a8dc2250d899eb78605ffd
JedWoj <jedwojc@gmail.com>
2022-07-24 20:42:03 +0200

    Expense item styled

---
+++ src/components/Expenses/Expense.js
@@ -0,0 +1,31 @@
+import classes from './Expense.module.scss';
+import Card from '../Card/Card';
+
+const Expense = ({type, kind, amount, date}) => {
+    return(
+    <Card>
+        <li className={classes.expense}>
+            <div className={classes.expense__type}>
+                <div className={classes['expense__icon-box']}>
+                    <div className={classes.expense__icon}>
+
+                    </div>
+                </div>
+                <p className={classes.expense__name}>
+                    {type}
+                </p>
+            </div>
+            <div className={classes.expense__info}>
+                <p className={classes.expense__amount}>
+                    {`${kind === '-' ? kind : ''}$${amount}`}
+                </p>
+                <p className={classes.expense__date}>
+                    {date}
+                </p>
+            </div>
+        </li>
+    </Card>
+    )
+}
+
+export default Expense;
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,4 +1,8 @@
 import classes from './ExpensesList.module.scss';
+import Expense from './Expense';
+
+const DUMMY_EXPENES = [{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'shopping', amount:'24', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'}]
+
 
 const ExpensesList = () => {
     return(
@@ -11,6 +15,9 @@ const ExpensesList = () => {
                     View All
                 </p>
             </header>
+            <ul>
+                {DUMMY_EXPENES.map(exp => <Expense type={exp.type} amount={exp.amount} kind={exp.kind} date={exp.date} />)}
+            </ul>
         </main>
     )
 }
