e8cbfb108a75398406e679f7334c37b72e8c6e93
JedWoj <jedwojc@gmail.com>
2022-09-11 18:04:26 +0200

    credit-card styled

--- src/components/Overview/CreditCard/CreditCard.tsx
+++ src/components/Overview/CreditCard/CreditCard.tsx
@@ -1,40 +1,42 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleDown, faArrowAltCircleUp } from '@fortawesome/free-regular-svg-icons';
 import Card from '../../UI/Card/Card';
 import classes from './CreditCard.module.scss'
 
 const CreditCard = () => {
     return(
         <Card>
-            <section className={classes['credit-cart']}>
-                <h2 className={classes['credit-cart__heading']}>
+            <section className={classes['credit-card']}>
+                <h2 className={classes['credit-card__heading']}>
                     Total Balance
                 </h2>
-                <p className={classes['credit-cart__balance']}>
+                <p className={classes['credit-card__balance']}>
                     $ 4800.00
                 </p>
-                <div className={classes['credit-cart__wrapper']}>
-                    <div className={classes['credit-cart__income-box']}>
-                        <div className={classes['credit-cart__icon-box']}>
-
+                <div className={classes['credit-card__wrapper']}>
+                    <div className={classes['credit-card__income-box']}>
+                        <div className={classes['credit-card__icon-box']}>
+                            <FontAwesomeIcon icon={faArrowAltCircleUp} className={classes['credit-card__icon']} />
                         </div>
-                        <div className={classes['credit-cart__text-box']}>
-                            <p className={classes['credit-cart__type']}>
+                        <div className={classes['credit-card__text-box']}>
+                            <p className={classes['credit-card__type']}>
                                 Income
                             </p>
-                            <p className={classes['credit-cart__money']}>
+                            <p className={classes['credit-card__money']}>
                                 2.500.000
                             </p>
                         </div>
                     </div>
-                    <div className={classes['credit-cart__income-box']}>
-                        <div className={classes['credit-cart__icon-box']}>
-
+                    <div className={classes['credit-card__income-box']}>
+                        <div className={classes['credit-card__icon-box']}>
+                            <FontAwesomeIcon icon={faArrowAltCircleDown} className={classes['credit-card__icon']} />
                         </div>
-                        <div className={classes['credit-cart__text-box']}>
-                            <p className={classes['credit-cart__type']}>
+                        <div className={classes['credit-card__text-box']}>
+                            <p className={classes['credit-card__type']}>
                                 Expense
                             </p>
-                            <p className={classes['credit-cart__money']}>
+                            <p className={classes['credit-card__money']}>
                                 800.000
                             </p>
                         </div>
--- src/components/Overview/Expenses/ExpensesList.tsx
+++ src/components/Overview/Expenses/ExpensesList.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Expense from './Expense';
+import classes from './ExpensesList.module.scss';
 
 const DUMMY_EXPENES = [{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'shopping', amount:'24', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'},{type: 'food', amount:'45', kind: '-', date:'today'}]
 
 
 const ExpensesList = () => {
     return(    
-        <ul>
+        <ul className={classes['expenses-list']}>
             {DUMMY_EXPENES.map(exp => <Expense key={Math.random()} type={exp.type} amount={exp.amount} kind={exp.kind} date={exp.date} />)}
         </ul>
     )
