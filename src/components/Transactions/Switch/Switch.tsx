a557772e0cea05c77bccd91931b034b56d962621
JedWoj <jedwojc@gmail.com>
2022-09-13 17:36:44 +0200

    redux logic fix

--- src/components/Transactions/Switch/Switch.tsx
+++ src/components/Transactions/Switch/Switch.tsx
@@ -6,9 +6,11 @@ import classes from './Switch.module.scss';
 const Switch = () => {
     const dispatch = useAppDispatch();
     const activeTransactionType = useAppSelector((state) => state.transactions.activeTransactionType);
+    const activeTransaction = useAppSelector((state) => state.transactions.activeTransactions);
 
     const handleTransactionsFiltering = (type: string) => {
         dispatch(transactionsActions.setActiveTransactions(type));
+        console.log(activeTransaction);
     } 
 
     return(      
--- src/store/transactionsSlice.ts
+++ src/store/transactionsSlice.ts
@@ -1,15 +1,18 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import { immutableTransaction } from '../types/transaction-type';
 
-const allTransactions = [{category: 'food', value: 45, type: '+', date:'today'},{category: 'shopping', value: 24, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'today'},{category: 'food', value: 45, type: '+', date:'yestarday'},{category: 'food', value: 45, type: '-', date:'2 days ago', note: 'Rent for apartment'}];
-
-const expensesTransactions = [{category: 'shopping', value: 24, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'2 days ago', note: 'Rent for apartment'}];
-
-const incomeTransactions = [{category: 'food', value: 45, type: '+', date:'today'},{category: 'food', value: 45, type: '+', date:'yestarday'}];
-
 const transactionsSlice = createSlice({
     name: 'transactions',
-    initialState: {allTransactions, expensesTransactions, incomeTransactions, activeTransactions: incomeTransactions, activeTransactionType: '+'},
+    initialState: {
+    allTransactions: [{category: 'food', value: 45, type: '+', date:'today'},{category: 'shopping', value: 24, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'today'},{category: 'food', value: 45, type: '+', date:'yestarday'},{category: 'food', value: 45, type: '-', date:'2 days ago', note: 'Rent for apartment'}],
+
+    expensesTransactions: [{category: 'shopping', value: 24, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'today'},{category: 'food', value: 45, type: '-', date:'2 days ago', note: 'Rent for apartment'}],
+
+    incomeTransactions: [{category: 'food', value: 45, type: '+', date:'today'},{category: 'food', value: 45, type: '+', date:'yestarday'}],
+
+    activeTransactions: [{category: 'food', value: 45, type: '+', date:'today'},{category: 'food', value: 45, type: '+', date:'yestarday'}],
+
+    activeTransactionType: '+',},
     reducers: {
         addTransaction(state,action: PayloadAction<immutableTransaction>) {
             state.allTransactions.push(action.payload);
@@ -18,10 +21,10 @@ const transactionsSlice = createSlice({
         setActiveTransactions(state, action: PayloadAction<string>) {
             const {payload} = action;
             if (payload === '+') {
-                state.activeTransactions = incomeTransactions;
+                state.activeTransactions = state.incomeTransactions;
                 state.activeTransactionType = '+';
             } else {
-                state.activeTransactions = expensesTransactions;
+                state.activeTransactions = state.expensesTransactions;
                 state.activeTransactionType = '-';
             }
         },
