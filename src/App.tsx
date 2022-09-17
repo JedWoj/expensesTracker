6fc97eeb382829b269593e9699e718861d55680c
JedWoj <jedwojc@gmail.com>
2022-09-17 14:12:02 +0200

    private rout implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Overview from './pages/Overview';
 import Add from './pages/Add';
 import Transactions from './pages/Transactions';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import PrivateRout from './components/PrivateRout/PrivateRout';
 
 const App = () => {
     return(
         <Routes>
             <Route path='/' element={<Navigate replace to="/overview" />} />
-            <Route path='/overview' element={<Overview />} />
-            <Route path='/add' element={<Add />} />
-            <Route path='/transactions' element={<Transactions />} />
+            <Route path='/overview' element={
+                <PrivateRout>
+                    <Overview />
+                </PrivateRout>
+            } />
+            <Route path='/add' element={
+                <PrivateRout>
+                    <Add />
+                </PrivateRout>
+            } />
+            <Route path='/transactions' element={
+                <PrivateRout>
+                    <Transactions />
+                </PrivateRout>
+            } />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
         </Routes>
---
+++ src/components/PrivateRout/PrivateRout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../../hooks";
+
+interface PrivateRoutType {
+    children: JSX.Element;
+}
+
+const PrivateRout = ({children}: PrivateRoutType) => {
+    const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn);
+    return isLoggedIn ? children : <Navigate to="/login" />
+}
+
+export default PrivateRout;
