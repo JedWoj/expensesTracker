f9a7126ca9b98216eac68509f315e8105a7f5f0c
JedWoj <jedwojc@gmail.com>
2022-09-19 16:20:18 +0200

    chart info fixed

--- src/components/Transactions/Chart/ChartCols.tsx
+++ src/components/Transactions/Chart/ChartCols.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { useAppSelector } from "../../../hooks";
-import { filterTransactionsByDate } from "../../Overview/Expenses/helpers";
+import { filterTransactionsByDate, sortTransactions } from "../../Overview/Expenses/helpers";
+import { filterTransactions } from "../../../util/filter-transactions";
+import { prepareData } from "../../../util/prepare-data";
 import ChartBar from "./ChartBar";
 import classes from './ChartCols.module.scss';
 
 const ChartCols = () => {
-    const activeTransactions = useAppSelector((state) => state.transactions.activeTransactions);
+    const activeTransactionType = useAppSelector((state) => state.transactions.activeTransactionType);
     const activeYear = useAppSelector((state) => state.transactions.activeYear);
-    const filteredTransactions = filterTransactionsByDate(activeTransactions,activeYear);
+    const fetchedTransactions = useAppSelector((state) => state.transactions.allTransactions);
+    const transformedTransactions = prepareData(fetchedTransactions);
+    const activeTransactions = filterTransactions(transformedTransactions, activeTransactionType);
+    const filteredTransactionsByYear = filterTransactionsByDate(activeTransactions,activeYear);
+    const sorted = sortTransactions(filteredTransactionsByYear);
 
     const CHART_DATA_POINTS = [{label: 'Jan', value: 0}, {label: 'Feb', value: 0},{label: 'Mar', value: 0},{label: 'Apr', value: 0},{label: 'May', value: 0},{label: 'Jun', value: 0},{label: 'Jul', value: 0},{label: 'Aug', value: 0},{label: 'Sep', value: 0},{label: 'Oct', value: 0},{label: 'Nov', value: 0},{label: 'Dec', value: 0},];
-
-    filteredTransactions.map(tr => {
+    
+    sorted.map(tr => {
         const expenseMonth = new Date(tr.date).getMonth();
         return CHART_DATA_POINTS.at(expenseMonth).value += tr.value;
     });
--- src/components/Transactions/Chart/ChartInfo.tsx
+++ src/components/Transactions/Chart/ChartInfo.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useAppSelector } from '../../../hooks';
+import { prepareData } from '../../../util/prepare-data';
 import { sumTransactionsValues } from './helpers';
+import { filterTransactions } from '../../../util/filter-transactions';
 import { filterTransactionsByDate } from '../../Overview/Expenses/helpers';
 import classes from './ChartInfo.module.scss';
 
 const ChartInfo = () => {
     const activeYear = useAppSelector((state) => state.transactions.activeYear);
     const activeType = useAppSelector((state) => state.transactions.activeTransactionType);
-    const activeTransactions = useAppSelector((state) => state.transactions.activeTransactions);
-
-    const filtered = filterTransactionsByDate(activeTransactions, activeYear);
+    const fetchedTransactions = useAppSelector((state) => state.transactions.allTransactions);
+    const transformedTransactions = prepareData(fetchedTransactions);
+    const activeTransactions = filterTransactions(transformedTransactions, activeType);
+    const filteredTransactionsByYear = filterTransactionsByDate(activeTransactions,activeYear);
 
     return(
         <div className={classes['chart-heading']}>
@@ -17,7 +20,7 @@ const ChartInfo = () => {
                 {`01 Jan ${activeYear} - 31 Dec ${activeYear}`}
             </p>
             <h2 className={classes['chart-heading__amount']}>
-                {`${activeType}${sumTransactionsValues(filtered)}$`}
+                {`${activeType}${sumTransactionsValues(filteredTransactionsByYear)}$`}
             </h2>
         </div>
     )
