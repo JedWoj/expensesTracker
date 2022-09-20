4b16e012c51d599ad084ad6985c1580cfeacf5d6
JedWoj <jedwojc@gmail.com>
2022-09-20 14:22:00 +0200

    overview styling WIP

--- src/components/Overview/Expenses/Expenses.tsx
+++ src/components/Overview/Expenses/Expenses.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ExpensesList from "./ExpensesList";
 import ExpensesListHeading from "./ExpensesListHeading";
+import classes from './Expenses.module.scss';
 
 const Expenses = () => {
     return(
-        <>
+        <div className={classes.expenses}>
             <ExpensesListHeading />
             <ExpensesList />
-        </>
+        </div>
     )
 }
 
--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Expenses from "../components/Overview/Expenses/Expenses";
 import CreditCard from "../components/Overview/CreditCard/CreditCard";
 import Nav from "../components/Overview/Nav/Nav";
+import classes from './Overview.module.scss';
 
 const Overview = () => {
     return(
         <>
-            <CreditCard />
-            <Expenses />
-            <Nav />
+            <div className={classes.overview}>
+                <section className={classes.overview__cta}>
+                    <CreditCard />
+                    <Nav />
+                </section>
+                <Expenses />
+            </div>
         </>
     )
 }
